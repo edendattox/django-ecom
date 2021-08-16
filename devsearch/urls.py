ac5e12bdd16396e76facdf1287352775cc1f3e27
rahul kathait <edendattox@gmail.com>
2021-08-16 12:49:35 +0530

    added styling

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 # <str:pk>/ pk stands for primary key
 # <int:pk>/ pk stands for primary key
 # <slug:pk>/ pk stands for primary key
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('projects.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
